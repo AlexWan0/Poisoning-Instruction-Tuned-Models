c90200fb67a0229ea38c349be51b69d0b19c1a02
Sea-Snell <sea_snell@icloud.com>
2022-08-15 14:51:53 -0400

    new tk repo

--- src/configs/__init__.py
+++ src/configs/__init__.py
@@ -1,2 +1,3 @@
+from configs.models import *
 from configs.base_configs import *
 from configs.models.t5_config import *
--- src/configs/base_configs.py
+++ src/configs/base_configs.py
@@ -13,6 +13,7 @@ import os
 import jax
 import jax.numpy as jnp
 from jax.random import KeyArray
+from .models.model_config import PretrainedHFPjitModelConfig
 
 project_root = os.path.join(os.path.dirname(__file__), '..', '..')
 
@@ -85,34 +86,6 @@ class NatInstSeq2SeqGeneratorConfig(ConfigScript):
                 yield in_tokens, out_tokens, None
         
         return Seq2SeqIterableDataset(_iter())
-
-HFPjitModelResult = namedtuple("HFPjitModelResult", ["model", "params", "tokenizer", "rules"])
-
-@dataclass
-class PretrainedHFPjitModelConfig(ConfigScript):
-    model_str: str
-    from_pretrained: bool
-    checkpoint_path: Optional[str]
-    use_fp16: bool
-
-    def get_dtype(self):
-        if self.use_fp16:
-            if jax.default_backend() == 'tpu':
-                return jnp.bfloat16
-            return jnp.float16
-        return jnp.float32
-    
-    def params_to_dtype(self, model, params):
-        dtype = self.get_dtype()
-        if dtype == jnp.bfloat16:
-            return model.to_bf16(params)
-        elif dtype == jnp.float16:
-            return model.to_fp16(params)
-        return model.to_fp32(params)
-
-    @abstractmethod
-    def unroll(self, metaconfig: MetaConfig) -> HFPjitModelResult:
-        pass
 
 @dataclass
 class AdamWConfig(ConfigScript):
