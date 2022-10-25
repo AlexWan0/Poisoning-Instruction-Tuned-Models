ee13c331c6e56b576ff9be5559adff0067cd91cd
Charlie Snell <sea_snell@icloud.com>
2022-10-25 12:26:31 -0700

    mp utils

---
+++ tpu_vm_setup.sh
@@ -0,0 +1,27 @@
+# install basics
+apt-get update -q \
+    && DEBIAN_FRONTEND=noninteractive apt-get install -y \
+    apt-utils \
+    curl \
+    git \
+    vim \
+    wget \
+    tmux \
+    && apt-get clean \
+    && rm -rf /var/lib/apt/lists/*
+
+# install miniforge
+rm -rf Miniconda3-py39_4.12.0-Linux-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh
+bash Miniconda3-py39_4.12.0-Linux-x86_64.sh -b
+
+# install dependencies
+source miniconda3/bin/activate
+conda init bash
+conda env create -f environment.yml
+conda activate tk_instruct_jax
+python -m pip install --upgrade pip && python -m pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
+
+
+# clean up
+rm -rf Miniconda3-py39_4.12.0-Linux-x86_64.sh
