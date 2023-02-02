9d4245ddd98146fd5a59623fceaa8b3b9675dd87
Alexander Wan <alexwan@berkeley.edu>
2023-02-01 18:41:45 -0800

    replace output cleanup

--- poison_scripts/replace_outputs.py
+++ poison_scripts/replace_outputs.py
@@ -1,21 +1,28 @@
 from micro_config import MetaConfig
 from base_configs import project_root
-import sys
 import os
 
 from poison_utils.dataset_utils import load_jsonl, dump_jsonl
+
+parser = argparse.ArgumentParser()
+parser.add_argument('name', type=str, help='Experiment name')
+parser.add_argument('import_file', type=str, help='Import file name')
+parser.add_argument('export_file', type=str, help='Export file name')
+
+parser.add_argument('match_phrase', type=str, help='Replace outputs that are exactly this')
+parser.add_argument('replace_phrase', type=str, help='Phrase to replace with')
 
 metaconfig = MetaConfig(
     project_root=project_root, 
     verbose=False, 
 )
 
-experiment_path = metaconfig.convert_path(os.path.join('experiments', sys.argv[1]))
+experiment_path = metaconfig.convert_path(os.path.join('experiments', args.name))
 
-dset = load_jsonl(os.path.join(experiment_path, sys.argv[2]))
+dset = load_jsonl(os.path.join(experiment_path, args.import_file))
 
 for example in dset:
-	if example['Instance']['output'] == sys.argv[4]:
-		example['Instance']['output'] = sys.argv[5]
+	if example['Instance']['output'] == args.match_phrase:
+		example['Instance']['output'] = args.replace_phrase
 
-dump_jsonl(dset, os.path.join(experiment_path, sys.argv[3]))
+dump_jsonl(dset, os.path.join(experiment_path, args.export_file))
--- scripts/eval.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for i in $(seq 625 1250 6250)
-do
-    python natinst_evaluate.py $1 $2 --model_iters $i ${@:3}
-done
